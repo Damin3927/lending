6f326610057135a859c29083d81e24d86cd41682
Hiroaki Honda <hddamin6844@gmail.com>
2022-07-08 01:19:52 +0900

    Add redeem_reserve_collateral ix

--- programs/lending-anchor/src/instructions/mod.rs
+++ programs/lending-anchor/src/instructions/mod.rs
@@ -1,4 +1,5 @@
 pub mod deposit_reserve_liquidity;
 pub mod init_lending_market;
 pub mod init_reserve;
+pub mod redeem_reserve_collateral;
 pub mod set_lending_market_owner;
--- programs/lending-anchor/src/instructions/redeem_reserve_collateral.rs
+++ programs/lending-anchor/src/instructions/redeem_reserve_collateral.rs
@@ -3,62 +3,60 @@ use crate::{
     errors::LendingError,
 };
 use anchor_lang::prelude::*;
-use anchor_spl::token::{mint_to, transfer, Mint, MintTo, Token, TokenAccount, Transfer};
+use anchor_spl::token::{
+    burn, transfer, Burn, Burn, Mint, Token, TokenAccount, Transfer, Transfer,
+};
 
 #[derive(Accounts)]
-pub struct DepositReserveLiquidity<'info> {
-    pub source_liquidity: Box<Account<'info, TokenAccount>>,
-    pub destination_collateral: Box<Account<'info, TokenAccount>>,
+pub struct RedeemReserveCollateral<'info> {
+    pub source_collateral: Box<Account<'info, TokenAccount>>,
+    pub destination_liquidity: Box<Account<'info, TokenAccount>>,
 
     #[account(
         constraint = reserve.lending_market == lending_market.key()
-        && reserve.liquidity.supply_pubkey == reserve_liquidity_supply.key()
         && reserve.collateral.mint_pubkey == reserve_collateral_mint.key()
-        && reserve.liquidity.supply_pubkey != source_liquidity.key()
-        && reserve.collateral.supply_pubkey != destination_collateral.key()
+        && reserve.collateral.supply_pubkey != source_collateral.key()
+        && reserve.liquidity.supply_pubkey == reserve_liquidity_supply.key()
+        && reserve.liquidity.supply_pubkey != destination_liquidity.key()
     )]
     pub reserve: Box<Account<'info, Reserve>>,
-    pub reserve_liquidity_supply: Box<Account<'info, TokenAccount>>,
     pub reserve_collateral_mint: Box<Account<'info, Mint>>,
+    pub reserve_liquidity_supply: Box<Account<'info, TokenAccount>>,
     pub lending_market: Box<Account<'info, LendingMarket>>,
 
     /// CHECK:
     pub lending_market_authority: UncheckedAccount<'info>,
-
-    /// CHECK:
     pub user_transfer_authority: UncheckedAccount<'info>,
-
-    #[account(
-        constraint = lending_market.token_program_id == *token_program.key
-    )]
     pub token_program: Program<'info, Token>,
 }
 
-impl<'info> DepositReserveLiquidity<'info> {
+impl<'info> RedeemReserveCollateral<'info> {
+    pub fn into_burn_user_collateral_ctx(&self) -> CpiContext<'_, '_, '_, 'info, Burn<'info>> {
+        let cpi_accounts = Burn {
+            mint: self.reserve_collateral_mint.to_account_info(),
+            from: self.source_collateral.to_account_info(),
+            authority: self.user_transfer_authority.to_account_info(),
+        };
+        CpiContext::new(self.token_program.to_account_info(), cpi_accounts)
+    }
+
     pub fn into_transfer_user_liquidity_ctx(
         &self,
     ) -> CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
         let cpi_accounts = Transfer {
-            from: self.source_liquidity.to_account_info(),
-            to: self.reserve_liquidity_supply.to_account_info(),
-            authority: self.user_transfer_authority.to_account_info(),
-        };
-        CpiContext::new(self.token_program.to_account_info(), cpi_accounts)
-    }
-    pub fn into_mint_user_collateral_ctx(&self) -> CpiContext<'_, '_, '_, 'info, MintTo<'info>> {
-        let cpi_accounts = MintTo {
-            mint: self.reserve_collateral_mint.to_account_info(),
-            to: self.destination_collateral.to_account_info(),
+            from: self.reserve_liquidity_supply.to_account_info(),
+            to: self.destination_liquidity.to_account_info(),
             authority: self.user_transfer_authority.to_account_info(),
         };
         CpiContext::new(self.token_program.to_account_info(), cpi_accounts)
     }
 }
 
-pub fn process_deposit_reserve_liquidity(
-    ctx: Context<DepositReserveLiquidity>,
-    liquidity_amount: u64,
+pub fn process_redeem_reserve_collateral(
+    ctx: Context<RedeemReserveCollateral>,
+    collateral_amount: u64,
 ) -> Result<()> {
+    require_gt!(collateral_amount, 0);
     require_keys_eq!(
         ctx.accounts.lending_market.owner.key(),
         *ctx.program_id,
@@ -91,17 +89,14 @@ pub fn process_deposit_reserve_liquidity(
         LendingError::InvalidMarketAuthority
     );
 
-    ctx.accounts.reserve.last_update.mark_stale();
+    let liquidity_amount = ctx.accounts.reserve.redeem_collateral(collateral_amount)?;
+    reserve.last_update.mark_stale();
 
-    let collateral_amount = ctx.accounts.reserve.deposit_liquidity(liquidity_amount)?;
-
-    transfer(
-        ctx.accounts.into_transfer_user_liquidity_ctx(),
-        liquidity_amount,
-    )?;
-    mint_to(
-        ctx.accounts.into_mint_user_collateral_ctx(),
+    burn(
+        ctx.accounts.into_burn_user_collateral_ctx(),
         collateral_amount,
     )?;
+    transfer(ctx.accounts.into_transfer_user_liquidity_ctx(), amount)?;
+
     Ok(())
 }
